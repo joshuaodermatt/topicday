90473df9ea4669b442a593471162163b3ae83e62
Noah <noah.buchs@outlook.com>
2023-10-27 10:01:03 +0200

    facemesh, size changed

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-const width = 800;
-const heigth = 600;
+const width = 640;
+const heigth = 480;
 
 let video;
+let facemesh;
+let predictions = [];
 
 
 function setup() {
     createCanvas(width, heigth);
     video = createCapture(VIDEO);
-    
     video.size(width, heigth);
+
+    facemesh = ml5.facemesh(video);
+
+    facemesh.on("predict", results => {
+        predictions = results;
+    });
 
     video.hide();
 }
 
-
 function draw() {
     image(video, 0, 0, width, heigth)
+    drawKeypoints()
+}
+
+function drawKeypoints() {
+    for (let i = 0; i < predictions.length; i += 1) {
+        const keypoints = predictions[i].scaledMesh;
+
+        // Draw facial keypoints.
+        for (let j = 0; j < keypoints.length; j += 1) {
+            const [x, y] = keypoints[j];
+
+            fill(0, 255, 0);
+            ellipse(x, y, 5, 5);
+        }
+    }
 }
